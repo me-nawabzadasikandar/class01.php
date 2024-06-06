a9f8ca3ae75a34245beed1336ff1bfe3a2cb1765
It_Me.sikandar._😊😎😛 <165310265+me-nawabzadasikandar@users.noreply.github.com>
2024-06-06 11:33:28 +0500

    Add files via upload

---
+++ class01.php
@@ -0,0 +1,35 @@
+?php
+
+echo("php practice");
+echo("<h1>php practice 1</h1>");
+
+echo("<script>alert('php arror');<script>");
+$num =10;
+$num1 =10;
+ echo($num + $num1);
+
+ $num2 =20;
+ $num3 =30;
+ echo($num2 - $num3);
+
+ $num4 =20;
+ $num5 =30;
+ echo($num4 * $num5);
+
+ $num6 =20;
+ $num7 =30;
+ echo($num6 / $num7);
+
+ $num8 =20;
+ $num9 =30;
+ echo($num8 % $num9);
+
+ $num10 =20;
+ $num11 =30;
+ echo($num10 > $num11);
+
+ $num12 =20;
+ $num13 =30;
+ echo($num12 < $num13);
+
+?>
